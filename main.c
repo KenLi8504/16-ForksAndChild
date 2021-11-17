034985aaffce8d5a2542aa3a44183ccd1af4fdab
KenLi8504 <kli22@stuy.edu>
2021-11-17 10:20:50 -0500

    Finished assignment?

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int childStuff(){
+  srand(time(NULL));
+  printf("The pid of this child is %d\n",getpid());
+  int sleepTime = rand()%4+2;
+  sleep(sleepTime);
+  printf("This child is done :)\n");
+  return sleepTime;
+}
+
+int main(){
+  srand(time(NULL));
+  printf("Starting the forking process...\n");
+  int firstSleep = 0;
+  int firstFork = fork();
+  if (firstFork != 0){
+    int randomnumber = 0;
+    int status = wait(&randomnumber);
+    printf("Parent: Child Process %d Slept For %d Seconds\n", status, WEXITSTATUS(status));
+    int secondFork = fork();
+    if (secondFork != 0){
+    int randomnumber = 0;
+    int status = wait(&randomnumber);
+    printf("Parent: Child Process %d Slept For %d Seconds\n", status, WEXITSTATUS(status));
+    printf("Everything is done :)\n");
+    return 0;
+    }
+  else{
+    int time = childStuff();
+    return time;
+  }
+}
+  else{
+    int time = childStuff();
+    return time;
+  }
+  return 0;
+}
